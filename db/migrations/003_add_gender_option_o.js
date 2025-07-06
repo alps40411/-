a83995f181bd0634c66b1ccf1ad380d37b8d8320
alps40411 <iainbo.lee@gmail.com>
2025-07-07 02:47:22 +0800

    O

--- db/migrations/003_add_gender_option_o.js
+++ db/migrations/003_add_gender_option_o.js
@@ -3,20 +3,32 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // 修改 administrators 表的 gender 欄位，添加 "O" 選項
-    await queryInterface.changeColumn("administrators", "gender", {
-      type: Sequelize.ENUM("M", "F", "O"),
-      allowNull: false,
-      defaultValue: "M",
-    });
+    // PostgreSQL 需要使用原生 SQL 來修改 ENUM 類型
+    await queryInterface.sequelize.query(
+      "ALTER TYPE \"enum_administrators_gender\" ADD VALUE 'O'"
+    );
   },
 
   async down(queryInterface, Sequelize) {
-    // 恢復原來的 gender 欄位設定，只有 "M" 和 "F"
-    await queryInterface.changeColumn("administrators", "gender", {
-      type: Sequelize.ENUM("M", "F"),
-      allowNull: false,
-      defaultValue: "M",
-    });
+    // PostgreSQL 不支援直接移除 ENUM 值，需要重新創建
+    // 1. 創建新的臨時 ENUM 類型
+    await queryInterface.sequelize.query(
+      "CREATE TYPE \"enum_administrators_gender_temp\" AS ENUM ('M', 'F')"
+    );
+
+    // 2. 修改欄位使用新的 ENUM 類型
+    await queryInterface.sequelize.query(
+      'ALTER TABLE administrators ALTER COLUMN gender TYPE "enum_administrators_gender_temp" USING gender::text::"enum_administrators_gender_temp"'
+    );
+
+    // 3. 刪除舊的 ENUM 類型
+    await queryInterface.sequelize.query(
+      'DROP TYPE "enum_administrators_gender"'
+    );
+
+    // 4. 重命名新的 ENUM 類型
+    await queryInterface.sequelize.query(
+      'ALTER TYPE "enum_administrators_gender_temp" RENAME TO "enum_administrators_gender"'
+    );
   },
 };
